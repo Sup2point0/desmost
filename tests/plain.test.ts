8b56afc8b3946f1d9827d8e81e75eba563946473
Sup#2.0 <sup2point0@gmail.com>
2026-08-05 21:07:49 +0100

    setup tests

---
+++ tests/plain.test.ts
@@ -0,0 +1,21 @@
+import { Parser, ParseResultKind } from "../src/parser";
+
+import { ltx } from "./shared";
+
+
+/**
+ * Check the parser doesn't mess with LaTeX absent of incantations.
+ */
+
+describe("preserves plain LaTeX", () =>
+{
+  test("1 line", () => {
+    let source = ltx `f\left(x\right)=\sin\left(x\right)`;
+    let parser = new Parser(source);
+    let result = parser.parse_next();
+    
+    assert.isNotNull(result);
+    assert.equal(result.kind, ParseResultKind.ExpressionState);
+    assert.equal(result.data, { latex: source });
+  });
+});
