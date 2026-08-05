820064447211b9e8eaf2e63fcce75fb43abc4ebb
Sup#2.0 <sup2point0@gmail.com>
2026-08-05 21:38:21 +0100

    split incantations into `Global` and `Local`

--- src/incantations.ts
+++ src/incantations.ts
@@ -1,7 +1,27 @@
 /**
- * The incantation invoked by a slash at the start of a line, such as `/viewport` or `/hidden`.
+ * An incantation that affects the entire Desmos calculator state.
  */
-export enum Incantation
+export enum GlobalIncantation
 {
-  VIEWPORT,
+  VIEWPORT = "viewport",
 }
+
+
+/**
+ * An incantation that affects only the expression following it.
+ */
+export enum LocalIncantation
+{
+  HIDDEN = "hidden",
+}
+
+
+/**
+ * An incantation, backed by its raw text representation.
+ * 
+ * Incantations are invoked by a slash at the start of a line, such as `/viewport` or `/hidden`.
+ */
+export type Incantation =
+  | GlobalIncantation
+  | LocalIncantation
+;
