1712aed6bf644911c9f723cd8c44d3c5f27618b6
Sup#2.0 <sup2point0@gmail.com>
2026-08-08 10:57:57 +0100

    add testing utils

---
+++ tests/parsing/full/shared.ts
@@ -0,0 +1,21 @@
+import { DesmostParser, ParseResult } from "../../../src/parser";
+
+
+export function assert_expression(result: ParseResult): asserts result is ParseResult.Expression
+{
+  assert.equal(result.kind, ParseResult.Kind.EXPRESSION);
+}
+
+export function assert_incantation(result: ParseResult): asserts result is ParseResult.IncantationInstance
+{
+  assert.equal(result.kind, ParseResult.Kind.INCANTATION);
+}
+
+
+export function expect_blank_line(parser: DesmostParser)
+{
+  let r = parser.parse_next();
+  assert_expression(r);
+  assert.equal(r.data.type, "expression");
+  assert.equal(r.data.latex, "");
+}
--- tests/shared.ts
+++ tests/shared.ts
@@ -1,1 +1,12 @@
 export const ltx = String.raw;
+
+
+/** Produce all ordered combinations of `left` and `right` (cartesian product) for testing. */
+export function matrix<P,Q>(left: P[], right: Q[]): Array<[P, Q]>
+{
+  if (left.length === 0 || right.length === 0) {
+    throw new Error(`Missing test cases!`);
+  }
+  
+  return left.flatMap(x => right.map(y => [x, y] as [P, Q]));
+}
