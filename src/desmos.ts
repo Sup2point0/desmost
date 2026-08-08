8810cc66de90945fe80647f14d919ce98b9e3965
Sup#2.0 <sup2point0@gmail.com>
2026-08-08 17:49:26 +0100

    polyfill desmos for server-side

---
+++ src/desmos.ts
@@ -0,0 +1,13 @@
+/**
+ * The Desmos API (currently) is browser-only, so we polyfill the API objects we need here for the server-side.
+ */
+
+export enum DesmosColour
+{
+  RED    = "#c74440",
+  BLUE   = "#2d70b3",
+  GREEN  = "#388c46",
+  PURPLE = "#6042a6",
+  ORANGE = "#fa7e19",
+  BLACK  = "#000000",
+}
