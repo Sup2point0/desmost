e6ba2023bd2dbd4fa7b0ecc93b10fcb508f14ae7
Sup#2.0 <sup2point0@gmail.com>
2026-08-08 21:43:21 +0100

    start implementing main compiler interface

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,58 @@
-import { DesmostParser, ParseResultKind } from "./parser";
+import { DesmostParser, ParseResult } from "./parser";
 
 
 /**
- * Parse the content in the given `source` Markdown code block, and inject the results into `desmos`.
+ * Compile Desmost into Desmos.
+ * 
+ * This calls the Desmost compiler: parsing `source`, and injecting the results into an existing `desmos` instance.
  */
-export function inject(
+export function compile(
   /** The Desmos calculator instance to modify. */
   desmos: Desmos.Calculator,
 
-  /** The Markdown code block containing the source LaTeX and special control commands. */
+  /** The Desmost source code. */
   source: string,
+
+  options: DesmostOptions,
 ): void
 {
   let parser = new DesmostParser(source);
 
   while (true) {
-    let result = parser.parse_next();
+    let block = parser.parse_next();
 
-    if (result == null) {
-      // TODO
-      continue;
+    if (block === ParseResult.DONE) {
+      break;
     }
     
-    if (result.kind === ParseResultKind.EXPRESSION) {
-      // TODO
+    switch (block.kind) {
+      // global incantation
+      case ParseResult.Kind.INCANTATION_INVOCATION:
+        let data = undefined;
+
+        if ("arg_raw" in block) {
+          data = block.incantation.evaluate_arg(block.arg_raw);
+        }
+
+        block.incantation.apply(desmos, data);
+        break;
+
+      // global incantation (error)
+      case ParseResult.Kind.INVALID_INCANTATION:
+        // TODO
+        break;
+
+      // expression
+      case ParseResult.Kind.EXPRESSION:
+        break;
     }
   }
+}
+
+
+/** Options to customise Desmost compilation. */
+interface DesmostOptions
+{
+  /** How should errors be handled? */
+  errors: "show" | "show-at-end" | "hide";
 }
