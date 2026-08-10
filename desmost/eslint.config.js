f3a39911d55d57e25da6a0d6844ea5ac6116adf6
Sup#2.0 <sup2point0@gmail.com>
2026-08-10 09:50:04 +0100

    compiler -- add eslint

---
+++ desmost/eslint.config.js
@@ -0,0 +1,53 @@
+import path from "node:path";
+
+import { defineConfig, includeIgnoreFile } from "eslint/config";
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import globals from "globals";
+
+
+export default defineConfig(
+  includeIgnoreFile(path.resolve(".gitignore")),
+  js.configs.recommended,
+  ts.configs.recommended,
+  {
+    files: ["**/*.{js,ts}"],
+    plugins: { js },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "no-implicit-coercion": "error",
+      "no-lonely-if": "error",
+      "no-regex-spaces": "off",
+      "no-sequences": "error",
+      "no-undef": "off",
+      "no-unmodified-loop-condition": "error",
+      "no-unneeded-ternary": "error",
+      "no-unreachable-loop": "error",
+      "no-unused-labels": "off",
+      "object-shorthand": "warn",
+      "prefer-const": "off",
+      "@typescript-eslint/explicit-member-accessibility": "error",
+      "@typescript-eslint/no-confusing-void-expression": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-unnecessary-condition": "error",
+      "@typescript-eslint/no-unnecessary-type-assertion": "error",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "error",
+      "@typescript-eslint/strict-boolean-expressions": "error",
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "(^_|^params$)", varsIgnorePattern: "^_" }
+      ],
+    },
+  },
+);
