2552485d613bdfaef05b46b1ae2ab37f6156ccbc
Sup#2.0 <sup2point0@gmail.com>
2026-08-13 21:55:05 +0100

    move rake scripts to `.autodoc/`

---
+++ .autodoc/doc-options.rb
@@ -0,0 +1,68 @@
+require "pathname"
+
+
+ROOT = Pathname(__dir__).parent
+SOURCE = ROOT / "desmost/src/compiler/options.ts"
+TARGET = ROOT / "docs/compiling/compiler-options.md"
+
+
+def main()
+   output = build_table(source: File.read(SOURCE))
+   text = File.read(TARGET)
+   text.gsub!(/(?<=<!-- autodoc\? -->\n).*?(?=\n<!-- autodoc\. -->)/ms, output)
+   File.write(TARGET, text)
+end
+
+def build_table(source:)
+   contents = source.match(/(?<=interface DesmostOptions\n\{).+(?=\n\}\n\n)/ms)[0].strip
+   fields = contents.split("\n\n")
+
+   data = fields.map { |text| extract_field(text:) }
+   rows = data.map { |data| build_table_row(*data) }
+
+   return "
+
+| Option | Values | Default | Description |
+| :----- | :----- | :------ | :---------- |
+#{rows.join("\n")}
+
+   ".strip
+end
+
+def extract_field(text:)
+   doc, src = text.strip.split(/\*\/\n +/)
+   ident, type = src.split("?: ")
+
+   case type
+      when "boolean" then values = ["true", "false"]
+      else values = type.split(" | ")
+   end
+
+   # extract default
+   default = doc.match(/(?<=Defaults to ).*?\./)&.[](0)
+   default ||= doc.match(/(?<=- ).+(?=\(default\))/)&.[](0)&.strip
+   default ||= "–"
+   if default.end_with?("`.")
+      default.chop!
+   end
+   
+   doc.gsub!(/Defaults to .*?\./, "")
+
+   # remove comment structure
+   doc.gsub!(/  +\* ?/, "")
+   doc.gsub!("/**", "")
+   doc.strip!
+   doc.gsub!("\n", "<br>")
+
+   return [ident, values, default, doc]
+end
+
+def build_table_row(ident, values, default, doc)
+   values = values.map { |v| "`#{v}`" }.join(" ")
+   doc.gsub!(/\n\n/, "<br><br>")
+
+   return "| **#{ident}** | #{values} | #{default} | #{doc} |"
+end
+
+
+main
