5a8a4e5c26b59157d422616c6b6d4a8c86d69d19
Sup#2.0 <sup2point0@gmail.com>
2026-08-18 20:09:30 +0100

    svelte -- fix compilation issues

--- desmost/svelte/desmost.svelte
+++ desmost/svelte/desmost.svelte
@@ -3,28 +3,64 @@
 Compile Desmost from Markdown.
 
 This transforms `<pre lang="desmos">` blocks into Desmos calculator embeds. It selects each `<pre lang="desmos">` block in its children, and for each constructs a `Desmos.GraphingCalculator` instance then calls the Desmost compiler on the source.
+
+When this component is unmounted, it calls `.destroy()` on each calculator instance to cleanup resources.
+
+## Example
+
+```svelte
+<script>
+  import Content from "./content.svx";
+</script>
+
+<Desmost>
+  <Content />
+</Desmost>
+```
+
+With compile options:
+
+```svelte
+<Desmost errors="crash">
+  <Content />
+</Desmost>
+```
+
+With styling:
+
+```svelte
+<Desmost width="100%" height="50vh">
+  <Content />
+</Desmost>
+```
 -->
 
 <script lang="ts">
 /// <reference types="desmos" />
 
-import { compile, DesmostOptions } from "../src";
+import { compile, type DesmostOptions } from "../src";
 
-import { onMount } from "svelte";
+import { onMount, type Snippet } from "svelte";
 
 
 interface Props extends DesmostOptions
 {
-  children: unknown;  // FIXME
-  style: string;
-  width: string;
-  height: string;
+  children: Snippet;
+
+  /** CSS styles to apply to each Desmos calculator's containing element. */
+  style?: string;
+
+  /** Width of each Desmos calculator's containing element. */
+  width?: string;
+  
+  /** Height of each Desmos calculator's containing element. */
+  height?: string;
 }
 
 let { children, style, width, height, ...options }: Props = $props();
 
 
-const root: HTMLElement;
+let root: HTMLElement;
 
 let desmos_instances: Desmos.Calculator[] = [];
 
@@ -34,6 +70,7 @@ onMount(() =>
 
   for (let source of sources.values()) {
     source.style.display = "none";
+    if (style != undefined) source.cssText = style;
 
     let el_desmos = source.parentNode!.insertBefore(
       document.createElement("div"),
@@ -56,6 +93,6 @@ onMount(() =>
 </script>
 
 
-<div bind:this={root}>
+<div bind:this={root} style:display="contents">
   {@render children?.()}
 </div>
