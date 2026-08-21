7202da0413f464ef931dee8d990c99e19c2d573a
Sup#2.0 <sup2point0@gmail.com>
2026-08-21 13:49:11 +0100

    compiler -- move `*_latex()` methods to `compiler/format.ts`

--- desmost/src/compiler/evaluate.ts
+++ desmost/src/compiler/evaluate.ts
@@ -1,4 +1,4 @@
-import { format_error } from "./format";
+import { format_error, normalise_latex, prettify_latex } from "./format";
 import type { DesmostOptions } from "./options";
 
 import { Ast } from "../parser";
@@ -116,36 +116,4 @@ export function evaluate_error(
         break;
     }
   }
-}
-
-
-/**
- * Remove line breaks from `latex` so Desmos can properly consume it.
- */
-function normalise_latex(latex: string): string
-{
-  latex = latex.replaceAll(/\s+/g, " ");
-  return latex;
-}
-
-
-/**
- * Prettify `latex` to render nicely in Desmos, reflecting how you would type directly into Desmos.
- * 
- * This includes:
- * 
- * - Replace `()`, `[]`, etc. with `\left(\right)`
- * - Replace `min()`, `max()`, etc. with `\operatorname{min}()`
- */
-function prettify_latex(latex: string): string
-{
-  latex = latex.replaceAll(/(?<!\\left)(\(|\[|\\\{)/g, "\\left$1");
-  latex = latex.replaceAll(/(?<!\\right)(\)|\]|\\\})/g, "\\right$1");
-
-  latex = latex.replaceAll(
-    /(?<=[^\w]|^)\\?(mean|median|count|total|repeat|join|sort|shuffle|unique|mod|ceil|floor|round|sign) ?(?=\(|\\left\()/g,
-    "\\operatorname{$1}"
-  );
-
-  return latex;
 }
--- desmost/src/compiler/format.ts
+++ desmost/src/compiler/format.ts
@@ -14,3 +14,35 @@ export function format_error(
   return `${prefix}${sep}${e.name}: ${e.message}`;
   // TODO maybe include stack? (configurable?)
 }
+
+
+/**
+ * Remove line breaks from `latex` so Desmos can properly consume it.
+ */
+export function normalise_latex(latex: string): string
+{
+  latex = latex.replaceAll(/\s+/g, " ");
+  return latex;
+}
+
+
+/**
+ * Prettify `latex` to render nicely in Desmos, reflecting how you would type directly into Desmos.
+ * 
+ * This includes:
+ * 
+ * - Replace `()`, `[]`, etc. with `\left(\right)`
+ * - Replace `min()`, `max()`, etc. with `\operatorname{min}()`
+ */
+export function prettify_latex(latex: string): string
+{
+  latex = latex.replaceAll(/(?<!\\left)(\(|\[|\\\{)/g, "\\left$1");
+  latex = latex.replaceAll(/(?<!\\right)(\)|\]|\\\})/g, "\\right$1");
+
+  latex = latex.replaceAll(
+    /(?<=[^\w]|^)\\?(mean|median|count|total|repeat|join|sort|shuffle|unique|mod|ceil|floor|round|sign) ?(?=\(|\\left\()/g,
+    "\\operatorname{$1}"
+  );
+
+  return latex;
+}
