25ebb4fe41336faa45c8080c2454d3881141c38e
Sup#2.0 <sup2point0@gmail.com>
2026-08-26 22:01:46 +0100

    【compiler】add details to errors

--- desmost/src/errors.ts
+++ desmost/src/errors.ts
@@ -10,28 +10,48 @@ export type NoMatch = typeof NO_MATCH & { readonly __brand?: unique symbol };
  * 
  * It's not *literally* unrecoverable in the sense that it will crash the compiler completely, but it means there are no further fallbacks to try.
  */
-export class UnrecoverableError extends Error {}
+export class UnrecoverableError extends Error
+{
+	details?: UnrecoverableError.Details;
+}
 
 export namespace UnrecoverableError
 {
+	export interface Details
+	{
+		hint?: string,
+		flagged_by?: string,
+	}
+
 	/** The parser unexpectedly reached the end of its entire input source code. */
-	export class UnexpectedEnd extends UnrecoverableError {}
+	export class UnexpectedEnd extends UnrecoverableError {
+		constructor(msg: string, override details?: Details) { super(msg); this.name = "Unexpected End"; }
+	}
 
 	/** The parser did not receive input that it expected. */
-	export class MissingInput extends UnrecoverableError {}
+	export class MissingInput extends UnrecoverableError {
+		constructor(msg: string, override details?: Details) { super(msg); this.name = "Missing Input"; }
+	}
 
 	/** The parser received input that did not match what it expected. */
-	export class UnexpectedInput extends UnrecoverableError {}
+	export class UnexpectedInput extends UnrecoverableError {
+		constructor(msg: string, override details?: Details) { super(msg); this.name = "Unexpected Input"; }
+	}
 
 	/** The parser received excess input that it did not expect. */
-	export class ExcessInput extends UnrecoverableError {}
-
+	export class ExcessInput extends UnrecoverableError {
+		constructor(msg: string, override details?: Details) { super(msg); this.name = "Excess Input"; }
+	}
 
 	/** An incantation can't be applied here. */
-	export class IllegalIncantation extends UnrecoverableError {}
+	export class IllegalIncantation extends UnrecoverableError {
+		constructor(msg: string, override details?: Details) { super(msg); this.name = "Illegal Incantation"; }
+	}
 
 	/** An incantation's argument couldn't be parsed. */
-	export class InvalidArgument extends UnrecoverableError {}
+	export class InvalidArgument extends UnrecoverableError {
+		constructor(msg: string, override details?: Details) { super(msg); this.name = "Invalid Argument"; }
+	}
 }
 
 
