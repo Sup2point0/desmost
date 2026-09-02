fe845f89fdc22267a930d38fe4af6cc22966cb70
Sup#2.0 <sup2point0@gmail.com>
2026-09-02 16:06:17 +0100

    【compiler】refactor `compile()` into `Compiler` class

--- desmost/src/compiler/compile.ts
+++ desmost/src/compiler/compile.ts
@@ -1,16 +1,6 @@
-import { evaluate_global_incantation, evaluate_expr, evaluate_error } from "./evaluate";
+import { DesmostCompiler, type DesmostDebug } from "./compiler";
 
 import { type DesmostOptions, fill_defaults } from "../options";
-import { DesmostParser, Ast } from "../parser";
-
-
-/** Debug diagnostics returned from `compile()`. */
-export interface DesmostDebug
-{
-	duration: number;
-	num_blocks: number;
-	ast: Ast[];
-}
 
 
 /**
@@ -55,114 +45,11 @@ export function compile(
 		return;
 	}
 
-	let opts = fill_defaults(options);
+	let compiler = new DesmostCompiler(
+		desmos,
+		source,
+		fill_defaults(options),
+	);
 
-	if (opts.debug) {
-		var t_init = performance.now();
-		var num_blocks = 0;
-		var ast: Ast[] = [];
-	}
-
-	let parser = new DesmostParser(source, opts);
-
-	let errors: string[] = [];
-	
-	/* To aggregate errors at the start, we need a target to retroactively inject errors into */
-	if (opts.place_errors === "start") {
-		desmos.setExpression({ id: "deferred-start", latex: " " });  // FIXME check if ` ` needed
-	}
-
-	/* Keep track of whether we're still evaluating leading blank expressions, which might be ignored. */
-	let seen_non_blank = false;
-
-	/** The number of pending blank expressions to add. */
-	let pending_blanks: number = 0;
-
-	/* The compiler is lazy, parsing and evaluating one block at a time (as opposed to first parsing the entire AST). We don't need the whole AST, so this saves memory. */
-	/* NOTE: It also doesn't sacrifice performance, since `.setExpressions()` internally just calls `.setExpression()` in a loop anyway. Benchmarks in the frontend produce similar times for both, so there's no performance improvement. */
-	try {
-		while (true) {
-			let r = parser.parse_next();
-			if (r === null) break;
-
-			if (opts.debug) {
-				num_blocks!++;
-				ast!.push(r);
-			}
-
-			/** An error message to leave for aggregation later. */
-			let defer: string | void = undefined;
-			
-			switch (r.kind) {
-				case Ast.Kind.INCANTATION_INVOCATION:
-					defer = evaluate_global_incantation(r, desmos, opts);
-					break;
-
-				case Ast.Kind.EXPRESSION:
-					// @ts-expect-error: check type-narrows
-					if (r.data.latex === " ") {
-						if (opts.ignore_all_blanks) break;
-						if (!seen_non_blank && !opts.keep_leading_blanks) break;
-						pending_blanks++;
-						break;
-					}
-					else {
-						flush_pending_blanks(desmos, pending_blanks);
-						pending_blanks = 0;
-						
-						seen_non_blank = true;
-						defer = evaluate_expr(r, desmos, opts);
-						break;
-					}
-
-				case Ast.Kind.INVALID_INCANTATION:
-					defer = evaluate_error(r.error, desmos, opts);
-					break;
-			}
-
-			if (defer !== undefined) {
-				errors.push(defer);
-			}
-		}
-	}
-	catch (e) {
-		errors.push((e as Error).message);
-	}
-
-	if (opts.keep_trailing_blanks) {
-		flush_pending_blanks(desmos, pending_blanks);
-	}
-
-	if (errors.length > 0) {
-		let expr: Desmos.ExpressionState = {
-			type: "text",
-			text: errors.join("\n\n"),
-		};
-
-		switch (opts.place_errors) {
-			case "start":
-				desmos.setExpression({ ...expr, id: "deferred-start" });
-				break;
-			case "end":
-			case "inline":
-				desmos.setExpression(expr);
-				break;
-		}
-	}
-
-	if (opts.debug) {
-		return {
-			duration: performance.now() - t_init!,
-			num_blocks: num_blocks!,
-			ast: ast!,
-		};
-	}
-}
-
-
-function flush_pending_blanks(desmos: Desmos.Calculator, count: number)
-{
-	for (let i = 0; i < count; i++) {
-		desmos.setExpression({ latex: ` ` });
-	}
+	return compiler.compile();
 }
---
+++ desmost/src/compiler/compiler.ts
@@ -0,0 +1,257 @@
+import dedent from "dedent";
+
+import { format_error, normalise_latex, prettify_latex } from "./format";
+
+import type { DesmostOptions } from "../options";
+import { DesmostError, type Unrecoverable } from "../errors";
+import { DesmostParser, Ast } from "../parser";
+import { is_latex } from "../desmos";
+
+
+/** Debug diagnostics returned from `compile()`. */
+export interface DesmostDebug
+{
+	duration: number;
+	num_blocks: number;
+	ast: Ast[];
+}
+
+
+export class DesmostCompiler
+{
+	private desmos: Desmos.Calculator
+
+	private source: string
+
+	private options: DesmostOptions
+
+	private parser: DesmostParser
+
+	private errors: DesmostError[] = []
+
+	/** Keep track of whether we're still evaluating leading blank expressions, which might be ignored. */
+	private seen_non_blank = false
+
+	/** The number of pending blank expressions to add. */
+	private pending_blanks: number = 0
+
+	private debug: DesmostDebug | null = null
+
+
+	constructor(
+		desmos: Desmos.Calculator,
+		source: string,
+		options: DesmostOptions,
+	)
+	{
+		this.desmos = desmos;
+		this.source = source;
+		this.options = options;
+		this.parser = new DesmostParser(this.source, this.options);
+	}
+
+
+	public compile(): Unrecoverable<void | DesmostDebug>
+	{
+		if (this.options.debug) {
+			this.debug = {
+				duration: performance.now(),
+				num_blocks: 0,
+				ast: [],
+			};
+		}
+
+		/* To aggregate errors at the start, we need a target to retroactively inject errors into */
+		if (this.options.place_errors === "start") {
+			this.desmos.setExpression({ id: "deferred", latex: " " });  // FIXME check if ` ` needed
+		}
+
+		/* The compiler is lazy, parsing and evaluating one block at a time (as opposed to first parsing the entire AST). We don't need the whole AST, so this saves memory. */
+		/* NOTE: It also doesn't sacrifice performance, since `.setExpressions()` internally just calls `.setExpression()` in a loop anyway. Benchmarks in the frontend produce similar times for both, so there's no performance improvement. */
+		try {
+			let done = false;
+
+			while (!done) {
+				done = this.compile_next();
+			}
+		}
+		catch (e) {
+			this.errors.push(e as Error);  // FIXME Should probably override as critical
+		}
+
+		if (this.options.keep_trailing_blanks) {
+			this.flush_pending_blanks();
+		}
+
+		/* NOTE: No need to check `options.place_errors`!
+
+			- If it's `start`, `id: deferred` will replace the placeholder at the start as required.
+			- If it's `end`, `id: deferred` won't exist so this expression will be appended as required.
+			- If it's `inline`, we shouldn't have any accumulated errors at all, but if we do, then the end is where they should be anyway.
+		*/
+		if (this.errors.length > 0) {
+			this.desmos.setExpression({ id: "deferred", type: "text", text: this.errors.join("\n\n") });
+		} else {
+			this.desmos.removeExpression({ id: "deferred" });
+		}
+
+		if (this.options.debug) {
+			this.debug!.duration = performance.now() - this.debug!.duration;
+
+			return this.debug!;
+		}
+	}
+
+	/**
+	 * Compile the next block, returning `true` if compilation has finished.
+	 */
+	compile_next(): Unrecoverable<boolean>
+	{
+		let result = this.parser.parse_next();
+		if (result === undefined) return true;
+
+		let { blocks, errors } = result;
+
+		let block = blocks[0];  // TODO future foreach
+
+		if (this.options.debug && block !== null) {
+			this.debug!.num_blocks++;
+			this.debug!.ast.push(block);
+		}
+		
+		if (block !== null) switch (block.kind)
+		{
+			case Ast.Kind.INCANTATION_INVOCATION:
+				this.evaluate_global_incantation(block);
+				break;
+
+			case Ast.Kind.EXPRESSION:
+				// @ts-expect-error: check type-narrows
+				if (block.data.latex === " ") {
+					if (this.options.ignore_all_blanks) break;
+					if (!this.seen_non_blank && !this.options.keep_leading_blanks) break;
+					this.pending_blanks++;
+					break;
+				}
+				else {
+					this.flush_pending_blanks();
+					this.pending_blanks = 0;
+					
+					this.seen_non_blank = true;
+					this.evaluate_expr(block);
+					break;
+				}
+		}
+
+		for (let error of errors) {
+			this.evaluate_error(error);
+		}
+
+		return false;
+	}
+
+
+	// == LOW-LEVEL == //
+
+	/**
+	 * Evaluate arguments (if any) to a global incantation `invocation`, then apply the incantation to `desmos`.
+	 */
+	evaluate_global_incantation(invocation: Ast.IncantationInvocation): Unrecoverable<void>
+	{
+		let data = undefined;
+
+		if ("arg_raw" in invocation && invocation.arg_raw != undefined) {
+			try {
+				data = invocation.incantation.evaluate_arg(invocation.arg_raw, this.options);
+			}
+			catch (e) {
+				this.evaluate_error(e as Error);
+			}
+		}
+
+		invocation.incantation.apply(this.desmos, data);
+	}
+
+
+	/**
+	 * Evaluate local incantation invocations on `expr`, then add `expr` to `desmos`.
+	 */
+	evaluate_expr(expr: Ast.Expression): Unrecoverable<void>
+	{
+		for (let invocation of expr.incantations) {
+			if (invocation.kind === Ast.Kind.INVALID_INCANTATION) {
+				this.evaluate_error(invocation.error);
+				continue;
+			}
+
+			let data = undefined;
+
+			if ("arg_raw" in invocation && invocation.arg_raw != undefined) {
+				try {
+					data = invocation.incantation.evaluate_arg(invocation.arg_raw, this.options);
+				}
+				catch (e) {
+					this.evaluate_error(e as Error);
+					// if it needs the arg, then we can't press on
+					if (invocation.incantation.requires_arg) continue;
+				}
+			}
+
+			try {
+				invocation.incantation.apply(expr.data, data);
+			}
+			catch (e) {
+				this.evaluate_error(e as Error);
+			}
+		}
+
+		if (is_latex(expr.data)) {
+			expr.data.latex = normalise_latex(expr.data.latex!);
+
+			if (this.options.prettify) {
+				expr.data.latex = prettify_latex(expr.data.latex);
+			}
+		}
+		else if (expr.data.type === "text" && this.options.dedent_text) {
+			expr.data.text = dedent(expr.data.text ?? "");
+		}
+
+		this.desmos.setExpression(expr.data);
+	}
+
+	/**
+	 * Handle a `DesmostError`, respecting the user's options.
+	 */
+	evaluate_error(error: DesmostError): Unrecoverable<void | string>
+	{
+		switch (this.options.errors) {
+			case "crash":
+				throw error;
+
+			case "suppress":
+				console.error(format_error(error, this.options));
+				break;
+
+			default: switch (this.options.place_errors) {
+				case "end":
+				case "start":
+					this.errors.push(error);
+					break;
+				
+				case "inline":
+					this.desmos.setExpression({
+						type: "text",
+						text: format_error(error, this.options),
+					});
+					break;
+			}
+		}
+	}
+
+	flush_pending_blanks()
+	{
+		for (let i = 0; i < this.pending_blanks; i++) {
+			this.desmos.setExpression({ latex: ` ` });
+		}
+	}
+}
