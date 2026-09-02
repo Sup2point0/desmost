f78e11d4ce93fd9d56ba24f464a53ddbc205e225
Sup#2.0 <sup2point0@gmail.com>
2026-09-02 16:53:05 +0100

    【compiler】polyfill `Desmos` global in tests

---
+++ desmost/tests/globals.ts
@@ -0,0 +1,15 @@
+import { beforeAll } from "vitest";
+
+
+beforeAll(() => {
+	global.Desmos = {
+		Colors: {
+			RED:    "#c74440",
+			BLUE:   "#2d70b3",
+			GREEN:  "#388c46",
+			PURPLE: "#6042a6",
+			ORANGE: "#fa7e19",
+			BLACK:  "#000000",
+		},
+	};
+});
--- desmost/vitest.config.js
+++ desmost/vitest.config.js
@@ -5,5 +5,6 @@ export default defineConfig(
 {
 	test: {
 		globals: true,
+		setupFiles: ["./tests/globals.ts"],
 	},
 });
