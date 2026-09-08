ab16d473fe872c3669bbbbbc7bac851f13969e7d
Sup#2.0 <sup2point0@gmail.com>
2026-09-08 13:58:31 +0100

    【decompiler】skip `""` empty fields, not just `undefined`

--- desmost/src/magic/incantation.ts
+++ desmost/src/magic/incantation.ts
@@ -147,7 +147,7 @@ export abstract class ArgIncantation<
 	 */
 	protected emit_object_arg(data: object): string
 	{
-		let defined = Object.fromEntries(Object.entries(data).filter((k, v) => v != undefined));
+		let defined = Object.fromEntries(Object.entries(data).filter((k, v) => Boolean(v)));
 		
 		return Json5.stringify(defined).slice(1, -1);
 	}
