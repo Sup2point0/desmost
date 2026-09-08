ccf842be05c340201f25c4c78a618c7b650d5323
Sup#2.0 <sup2point0@gmail.com>
2026-09-08 14:45:44 +0100

    【compiler】add tests

---
+++ desmost/tests/compiling/full/easy.test.ts
@@ -0,0 +1,29 @@
+import { compile } from "../../../src";
+
+import { ltx, testing_desmos } from "../../shared";
+import { assert_no_errors } from "../shared";
+import { EASY } from "../../cases/easy";
+
+
+describe("easy", () =>
+{
+	test("compiles without errors", () => {
+		let desmos = testing_desmos();
+		compile(desmos, EASY);
+		assert_no_errors(desmos);
+	});
+
+	test("compiles correctly", () => {
+		let desmos = testing_desmos();
+		compile(desmos, EASY);
+		let exprs = desmos.getExpressions();
+	
+		let i = 0;
+		assert.equal(exprs[i++].latex, ltx `f\left(0\right) = 0`);
+		assert.equal(exprs[i++].latex, ltx `f\left(1\right) = 1`);
+		assert.equal(exprs[i++].latex, ltx `f\left(n\right) = f\left(n-1\right) + f\left(n\right)`);
+		assert.equal(exprs[i++].latex, ltx ` `);
+		assert.equal(exprs[i++].latex, ltx `\frac{1}{10} \sum_{n=1}^{10} \frac{f\left(n+1\right)}{f\left(n\right)}`);
+		assert.equal(exprs[i++], undefined);
+	});
+})
--- desmost/tests/parsing/full/single.test.ts
+++ desmost/tests/parsing/full/single.test.ts
@@ -1,7 +1,6 @@
-import { ViewportIncantation } from "../../../src/magic/global/viewport";
-import { ColourIncantation } from "../../../src/magic/local/colour";
-
 import { DesmostParser, Ast } from "../../../src/parser";
+import { GLOBAL_INCANTATIONS, LOCAL_INCANTATIONS } from "../../../src/magic";
+
 import { is_expr, is_invoc, parses_block } from "../shared";
 
 
@@ -13,20 +12,20 @@ test("/latex", () => {
 	assert.isNotEmpty(r.data.latex);
 	assert.equal(r.data.latex, "y = x^2")
 	assert.deepEqual(r.incantations, []);
-})
+});
 
 test("/viewport", () => {
-	let parser = new DesmostParser(`/viewport{ left: -1, right: 1 }`);
+	let parser = new DesmostParser(`/viewport{left: -1, right: 1}`);
 	let r = parses_block(parser);
 
 	is_invoc(r);
 	assert.equal(r.kind, Ast.Kind.INCANTATION_INVOCATION);
-	assert.deepEqual(r.incantation, new ViewportIncantation());
+	assert.deepEqual(r.incantation, GLOBAL_INCANTATIONS.viewport);
 	assert.deepEqual(r.arg_raw, "left: -1, right: 1");
-})
+});
 
 test("/colour", () => {
-	let parser = new DesmostParser(`/colour{ BLUE } :: y = x^2`);
+	let parser = new DesmostParser(`/colour{BLUE} :: y = x^2`);
 	let r = parses_block(parser);
 
 	is_expr(r);
@@ -34,6 +33,6 @@ test("/colour", () => {
 	assert.equal(r.incantations.length, 1);
 	
 	let invocation = r.incantations[0];
-	assert.deepEqual(invocation.incantation, new ColourIncantation());
+	assert.deepEqual(invocation.incantation, LOCAL_INCANTATIONS.colour);
 	assert.equal(invocation.arg_raw, "BLUE")
-})
+});
