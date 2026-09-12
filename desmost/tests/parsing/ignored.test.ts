b64b7c4053d81822b657c54669c1f78aef0de995
Sup#2.0 <sup2point0@gmail.com>
2026-09-12 10:54:27 +0100

    【compiler】add tests for tabs

--- desmost/tests/parsing/ignored.test.ts
+++ desmost/tests/parsing/ignored.test.ts
@@ -3,6 +3,38 @@ import { DesmostParser, Ast } from "../../src/parser";
 import { parses_block } from "./shared";
 
 
+describe("ignores tab", () =>
+{
+	test.each([
+		`1\n\t2`,
+		`1\t\n2`,
+		`1\t\n2\t`,
+	])
+	("as indentation", src => {
+		let parser = new DesmostParser(src);
+		let r: Ast;
+
+		r = parses_block(parser);
+		assert.equal(r.data.latex, `1`);
+		r = parses_block(parser);
+		assert.equal(r.data.latex, `2`);
+	});
+
+	test.each([
+		`\t\nx`,
+		`\t\t\nx`,
+	])
+	("as empty", src => {
+		let parser = new DesmostParser(src);
+		let r: Ast;
+
+		r = parses_block(parser);
+		assert.equal(r.data.latex, ` `);
+		r = parses_block(parser);
+		assert.equal(r.data.latex, `x`);
+	});
+})
+
 test("ignores CR", () => {
 	let parser = new DesmostParser(`y\r\nx`);
 	let r: Ast;
