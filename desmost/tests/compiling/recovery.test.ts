3d3e333f3a3b774ca6377edc6784af59af2bf092
Sup#2.0 <sup2point0@gmail.com>
2026-09-12 15:01:13 +0100

    【compiler】make recovery tests stricter

--- desmost/tests/compiling/recovery.test.ts
+++ desmost/tests/compiling/recovery.test.ts
@@ -1,4 +1,5 @@
 import { compile } from "../../src";
+import * as utils from "../../src/utils";
 
 import { testing_desmos } from "../shared";
 import { assert_has_errors } from "./shared";
@@ -7,49 +8,66 @@ import { assert_has_errors } from "./shared";
 /**
  * Test that compiling `src` fails gracefully, meaning it still flags errors, but finishes parsing to the end.
  */
-function test_cases(name: string, cases: string[])
+function test_cases(...expected_exprs: string[])
 {
-	test.each(cases)(name, src => {
+	return (src: string) =>
+	{
 		let desmos = testing_desmos();
 		compile(desmos, src + "\ndone");
 		assert_has_errors(desmos);
 
 		let exprs = desmos.getExpressions();
+
 		assert.equal(exprs.at(-1)!.latex, `done`);
-	});
+
+		if (expected_exprs != undefined) {
+			let i = 0;
+
+			for (let expr of exprs) {
+				if (expr.latex != undefined) {
+					assert.equal(expr.latex, expected_exprs[i]);
+					i++;
+					if (i >= expected_exprs.length) break;
+				}
+			}
+		}
+	};
 }
 
 
 describe("excess input", () =>
 {
-	test_cases("1 line", [
+	test.each([
 		`/hide extra :: x`,
 		`/colour{BLUE} extra :: x`,
 		`/line{opacity: 0.5} extra :: x`,
 		`/line{opacity: 0.5, extra} extra :: x`,
-	]);
+	])
+	("1 line", test_cases(`x`));
 	
-	test_cases("multi line", [
+	test.each([
 		`/hide extra :: x\n/hide :: y`,
 		`/hide extra :: x \n/hide :: y`,
 		`/hide extra :: x\n /hide :: y`,
 		`/hide extra :: x \n /hide :: y`,
-		`/hide\nextra :: x`,   `/hide\nextra ::\nx`,
-		`/hide \nextra :: x`,  `/hide \nextra ::\nx`,
-		`/hide\n extra :: x`,  `/hide\n extra ::\nx`,
-		`/hide \n extra :: x`, `/hide \n extra ::\nx`,
-		`/hide \n extra :: x`, `/hide \n extra ::\nx`,
-	]);
+		`/hide\nextra :: x`,   `/hide\nextra ::\ny`,
+		`/hide \nextra :: x`,  `/hide \nextra ::\ny`,
+		`/hide\n extra :: x`,  `/hide\n extra ::\ny`,
+		`/hide \n extra :: x`, `/hide \n extra ::\ny`,
+		`/hide \n extra :: x`, `/hide \n extra ::\ny`,
+	])
+	("multi line", test_cases(`x`, `y`));
 })
 
 describe("missing input", () =>
 {
-	test_cases("1 line", [
+	test.each([
 		`/viewport`,
 		`/colour :: x`,
-	]);
+	])
+	("1 line", test_cases());
 
-	test_cases("multi line", [
+	test.each([
 		`/viewport\nx`,
 		`/viewport \nx`,
 		`/viewport\n x`,
@@ -61,5 +79,6 @@ describe("missing input", () =>
 		`/colour \n/no-line\n :: x`,
 		`/colour\n /no-line \n:: x`,
 		`/colour \n /no-line \n :: x`,
-	]);
+	])
+	("multi line", test_cases(`x`));
 })
